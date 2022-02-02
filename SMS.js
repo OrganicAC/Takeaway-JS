b4a4ebbe245fefec416293d0e2c87822834ea39a
OrganicAC <anthonychaplin17@gmail.com>
2022-02-02 12:42:37 +0000

    added twilio functionality, dotenv setup, order class set up to send confirmation text

---
+++ SMS.js
@@ -0,0 +1,35 @@
+require('dotenv').config()
+const client = require('twilio')(process.env.accountSid, process.env.authToken);
+process.env.accountSid
+process.env.authToken
+process.env.from
+process.env.to 
+
+
+class Text {
+  constructor(accountSid, authToken, from, to) {
+    this.accountSid = accountSid
+    this.authToken = authToken
+    this.from = from
+    this.to = to
+  }
+
+  deliveryTime() {
+    var minutesToAdd=60;
+    var currentDate = new Date();
+    var futureDate = new Date(currentDate.getTime() + minutesToAdd*60000);
+    return String(futureDate).slice(16, 21)
+  }
+
+  sendSMS() {
+    client.messages
+  .create({
+    to: this.to,
+    from: this.from,
+    body: `Your order will arrive around ${this.deliveryTime()}`
+  })
+  .then(message => console.log(message.sid));
+  }
+}
+
+module.exports = Text;
--- order.js
+++ order.js
@@ -1,9 +1,12 @@
 const Menu = require('./menu');
+const Text = require('./SMS');
+require('dotenv').config()
 const menu = new Menu;
 
 class Order {
-  constructor() {
+  constructor(text = null) {
     this.basket = []; 
+    this.text = text
   }
 
   addItem(item, amount) {
@@ -22,12 +25,31 @@ class Order {
     
     };
       return total.toFixed(2);
-    
   }
 
   showSummary() {
+    console.log('Basket: ', this.basket)
     console.log('Total: £' + this.getTotal());
+  }
+
+  sendText() {
+    if (this.text != null){
+      this.text.sendSMS()
+    } else {
+      return "Enjoy your meal."
+    };
   }
 }
 
 module.exports = Order;
+
+const sms = new Text('AC3e60dbff97d7debb6e1c51bd227aa9bd', 'ab870d2bd806f9028882ed6d119c1879', '+18106418782', '+447779426575')
+const x = new Order(text = sms);
+x.sendText()
+
+
+//add 30 minutes to date
+// var minutesToAdd=60;
+// var currentDate = new Date();
+// var futureDate = new Date(currentDate.getTime() + minutesToAdd*60000);
+// console.log(String(futureDate).slice(16, 21))
